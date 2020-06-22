e81e3f51219a92111eb9e9bd4452ae2d7c3b12c3
Evan <igobyE@gmail.com>
2020-06-22 17:41:35 -0500

    get a jira question

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 
 # Need the following modules
 # python3 -m pip install python-dotenv
+# python3 -m pip install requests
 
 ###############################################################################
 import time
@@ -14,10 +15,43 @@ import os
 import sys
 from dotenv import load_dotenv
 load_dotenv()
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
 
 ###############################################################################
 #   Support functions
 ###############################################################################
+def printJson(data):
+    print(json.dumps(data, sort_keys=True, indent=4, separators=(",", ": ")))
+
+###############################################################################
+#   Get a jira question
+#   arguments:
+#       - jira ID: "QUES-6019"
+#   returns:
+#       json
+###############################################################################
+def getQuestion(jiraId):
+    url = os.environ.get('companyUrl')+"/rest/api/3/issue/"+jiraId
+    auth = HTTPBasicAuth(
+        os.environ.get('jiraUser'),
+        os.environ.get('api-token')
+    )
+
+    headers = {
+        "Accept": "application/json"
+    }
+
+    response = requests.request(
+        "GET",
+        url,
+        headers=headers,
+        auth=auth
+    )
+
+    return json.loads(response.text)
 
 ###############################################################################
 #   Testing
@@ -25,10 +59,15 @@ load_dotenv()
 def testing():
     print ("testing")
 
+    ## get a jira question
+    tmp = getQuestion("QUES-6019")
+    printJson(tmp)
+
+    sys.exit(0)
+
     ## testing environment variables
     print (os.environ.get('USER'))
     print (os.environ.get('api-token'))
-
 
     sys.exit(0)
 
