95caed3bb3f27d424f74022b92b1d0a9bf51d38c
Evan <igobyE@gmail.com>
2020-06-24 20:46:34 -0500

    moved code testing to testing.py

---
+++ testing.py
@@ -0,0 +1,136 @@
+###############################################################################
+#   Testing parts
+###############################################################################
+def testing():
+    print ("testing")
+
+    ## issueSearch
+    tmpData = {
+        "summary": "OSCAGc07s01*",
+        # "labels": ["CSULAWeek01"],
+        "fields": [
+            "key", "Question Type", "MD5 Spec Hash",
+            "UDB Update", "Math Class", "ID Number",
+            "Mathematica Specification", "LaTeX", "QSH1", "QSH2", "QSH3",
+            "Question Launch", "Source", "question stimumus",
+            "description", "StepWise label", "Querium product ID",
+            "EdX Specification"
+        ],
+        # "fields": ["key", "Question Type", "MD5 Spec Hash"],
+        # "fields": ["key"],
+        "qstnType": "StepWise"
+    }
+    tmp = issueSearch(tmpData, flatten=True)
+    printJson(tmp)
+    print("total len: ", len(tmp))
+
+    sys.exit(0)
+
+    ## combContents
+    tmp = {
+        "content": [
+            {
+                "content": [
+                    {
+                        "text": "Solve  by any method.  (\\begin{array}{c}",
+                        "type": "text"
+                    },
+                    {
+                        "type": "hardBreak"
+                    },
+                    {
+                        "text": "6x-8y=-0.6 ",
+                        "type": "text"
+                    }
+                ],
+                "type": "paragraph"
+            },
+            {
+                "content": [
+                    {
+                        "text": "3x+2y=0.9",
+                        "type": "text"
+                    },
+                    {
+                        "type": "hardBreak"
+                    },
+                    {
+                        "text": "\\end{array})",
+                        "type": "text"
+                    }
+                ],
+                "type": "paragraph"
+            }
+        ]
+    }
+    rslt = combContents(tmp)
+    print(rslt)
+
+    sys.exit()
+
+    ##getFieldValueV2
+    tmpField = "MD5 Spec Hash"
+    # tmpField = "Question Type"
+    tmp = getFieldValue(tmpField,
+        {
+            'expand': 'operations,versionedRepresentations,editmeta,changelog,renderedFields',
+            'id': '47356',
+            'self': 'https://querium.atlassian.net/rest/api/3/issue/47356',
+            'key': 'QUES-12889',
+            'fields': {
+                'customfield_11300': '569b8f88d5a3e3021ea4ead6b243b91a',
+                'customfield_11706': {
+                    'self': 'https://querium.atlassian.net/rest/api/3/customFieldOption/10704',
+                    'value': 'StepWise',
+                    'id': '10704'
+                }
+            }
+        })
+    print (tmp)
+
+    sys.exit()
+
+    ## fieldToCust
+    tmp = [fieldToCust(i) for i in ["key", "Question Type", "MD5 Spec Hash"]]
+    print(tmp)
+    sys.exit()
+
+    ## getFields
+    flds = ["a", "c"]
+    arr = [{"a": "one", "b": "two", "c":"three"}, {"a": "four", "b": "five"}]
+    print (getFields(flds, arr))
+
+    sys.exit()
+
+    ## fieldToCust
+    tmpField = "ID Number"
+    print(tmpField, ":", fieldToCust(tmpField))
+    tmpField = "Querium product ID"
+    print(tmpField, ":", fieldToCust(tmpField))
+    tmpField = "rand"
+    print(tmpField, ":", fieldToCust(tmpField))
+    sys.exit(0)
+
+    ## test: buildJql
+    tmpData = {
+        "summary": "OSCAGc07s01*",
+        "labels": ["CSULAWeek01"],
+        "qstnType": "StepWise",
+        "filds": ["key", "summary", "labels", "Question Type"]
+    }
+    print (buildJql("issueSearch", tmpData))
+    print (buildJql("xxx", {"a": "one", "b": "two"}))
+    sys.exit(0)
+
+
+    ## get a jira question
+    tmp = getQuestion({"issue": "QUES-6019"})
+    printJson(tmp)
+
+    sys.exit(0)
+
+    ## testing environment variables
+    print (os.environ.get('USER'))
+    print (os.environ.get('api-token'))
+
+    sys.exit(0)
