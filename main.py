9cc7a4a0850ed3919aabd99353207004af606ebd
Evan <igobyE@gmail.com>
2020-06-24 20:38:43 -0500

    postprocess issueSearch (combContents, flattenValue, getFieldValueV2, getFields) and handle custom fields

--- main.py
+++ main.py
@@ -69,6 +69,71 @@ def fieldToCust(field):
 
     return switcher.get(field, field)
 
+def combContents(content):
+    allText = ""
+    if isinstance(content, dict) and "content" in content:
+        allText += combContents(content["content"])
+    elif isinstance(content, list):
+        for part in content:
+            allText += combContents(part)
+    else:
+        return content["text"] if content["type"]=="text" else ""
+
+    return allText
+
+def flattenValue(field, item, dfltValue=None):
+    if item is None:
+        return dfltValue
+    if field == "Question Type":
+        return item["value"] if "value" in item else dfltValue
+    if field in [
+        "LaTeX", "QSH1", "QSH2", "QSH3", "question stimumus", "EdX Specification"
+    ]:
+        if "content" not in item:
+            return dfltValue
+
+        return combContents(item)
+
+    return item
+
+def getFieldValue(field, item, dfltValue=None, flatten=False):
+    customFields = [
+        "MD5 Spec Hash", "UDB Update", "Math Class", "ID Number",
+        "Mathematica Specification", "LaTeX", "QSH1", "QSH2", "QSH3",
+        "Question Launch", "Source", "Question Type", "question stimumus",
+        "description", "StepWise label", "Querium product ID",
+        "EdX Specification"
+    ]
+    if field in customFields and "fields" in item:
+        # print("getFieldValueV2 - 2")
+        if fieldToCust(field) in item["fields"]:
+            # print("getFieldValueV2 - 3")
+            if flatten==True:
+                return flattenValue(field, item["fields"][fieldToCust(field)])
+            return item["fields"][fieldToCust(field)]
+        else:
+            return dfltValue
+
+    # print("getFieldValueV2 - 4")
+    if fieldToCust(field) in item:
+        # print("getFieldValueV2 - 5")
+        return item[fieldToCust(field)]
+
+    return dfltValue
+
+# flds: ["a", "b"]
+# arr: [{"a": "one", "b": "two", "c":"three"}, {"a": "four", "b": "five"}]
+def getFields(flds, arr, flatten=False):
+    result = []
+    for item in arr:
+        itemResult = {}
+        for f in flds:
+            itemResult[f] = getFieldValue(f, item, flatten=flatten)
+
+        result.append(itemResult)
+
+    return result
+
 ###############################################################################
 #   Get a jira question
 #   arguments:
@@ -104,10 +169,11 @@ def getQuestion(data):
 #   arguments:
 #       - summary: "OSCAG"
 #       - labels: ["label1", "label2"]
+#       - [flatten]: False means return without postprocessing the value
 #   returns:
 #       json
 ###############################################################################
-def issueSearch(data):
+def issueSearch(data, flatten=False):
     route, jql = buildJql("issueSearch", data)
 
     url = os.environ.get('companyUrl')+route
@@ -123,10 +189,11 @@ def issueSearch(data):
         "jql": jql,
         "fieldsByKeys": False,
         "startAt": 0,
-        "maxResults": 10
+        "maxResults": 100
     }
     if("fields" in data):
-        payloadObj["fields"] = data["fields"]
+        payloadObj["fields"] = [fieldToCust(i) for i in data["fields"]]
+
 
     gotEverythingQ = False
     rsltAll = []
@@ -140,9 +207,11 @@ def issueSearch(data):
             auth=auth
         )
 
+        # If request fails, return the error code
         if response.status_code != 200:
             return response.status_code
 
+        # Exiting the while loop conditions
         rsltPrt = json.loads(response.text)
         if "total" not in rsltPrt:
             gotEverythingQ = True
@@ -150,21 +219,16 @@ def issueSearch(data):
             gotEverythingQ = True
         if "maxResults" not in rsltPrt:
             gotEverythingQ = True
+        # If there are more data to retrieve increment the startAt value
         if (rsltPrt["startAt"]+rsltPrt["maxResults"]) < rsltPrt["total"]:
             payloadObj["startAt"] = rsltPrt["startAt"]+rsltPrt["maxResults"]
         else:
             gotEverythingQ = True
 
-        print("maxResults:", rsltPrt["maxResults"])
-        print("startAt:", rsltPrt["startAt"])
-        print("total:", rsltPrt["total"])
         if "issues" in rsltPrt:
-            # rsltAll.extend(rsltPrt["issues"])
             if("fields" in data):
-                rsltAll.extend([
-                    {k:issue[fieldToCust(k)] for k in data["fields"]}
-                    for issue in rsltPrt["issues"]
-                ])
+                rsltAll.extend(getFields(data["fields"], rsltPrt["issues"],
+                flatten=flatten))
             else:
                 rsltAll.extend(rsltPrt["issues"])
 
@@ -179,17 +243,100 @@ def testing():
     ## issueSearch
     tmpData = {
         "summary": "OSCAGc07s01*",
-        "labels": ["CSULAWeek01"],
-        "fields": ["key", "Question Type", "MD5 Spec Hash"],
+        # "labels": ["CSULAWeek01"],
+        "fields": [
+            "key", "Question Type", "MD5 Spec Hash",
+            "UDB Update", "Math Class", "ID Number",
+            "Mathematica Specification", "LaTeX", "QSH1", "QSH2", "QSH3",
+            "Question Launch", "Source", "question stimumus",
+            "description", "StepWise label", "Querium product ID",
+            "EdX Specification"
+        ],
         # "fields": ["key", "Question Type", "MD5 Spec Hash"],
         # "fields": ["key"],
         "qstnType": "StepWise"
     }
-    tmp = issueSearch(tmpData)
+    tmp = issueSearch(tmpData, flatten=True)
     printJson(tmp)
     print("total len: ", len(tmp))
 
     sys.exit(0)
+
+    ## combContents
+    tmp = {
+        "content": [
+            {
+                "content": [
+                    {
+                        "text": "Solve  by any method.  (\\begin{array}{c}",
+                        "type": "text"
+                    },
+                    {
+                        "type": "hardBreak"
+                    },
+                    {
+                        "text": "6x-8y=-0.6 ",
+                        "type": "text"
+                    }
+                ],
+                "type": "paragraph"
+            },
+            {
+                "content": [
+                    {
+                        "text": "3x+2y=0.9",
+                        "type": "text"
+                    },
+                    {
+                        "type": "hardBreak"
+                    },
+                    {
+                        "text": "\\end{array})",
+                        "type": "text"
+                    }
+                ],
+                "type": "paragraph"
+            }
+        ]
+    }
+    rslt = combContents(tmp)
+    print(rslt)
+
+    sys.exit()
+
+    ##getFieldValueV2
+    tmpField = "MD5 Spec Hash"
+    # tmpField = "Question Type"
+    tmp = getFieldValue(tmpField,
+        {
+            'expand': 'operations,versionedRepresentations,editmeta,changelog,renderedFields',
+            'id': '47356',
+            'self': 'https://querium.atlassian.net/rest/api/3/issue/47356',
+            'key': 'QUES-12889',
+            'fields': {
+                'customfield_11300': '569b8f88d5a3e3021ea4ead6b243b91a',
+                'customfield_11706': {
+                    'self': 'https://querium.atlassian.net/rest/api/3/customFieldOption/10704',
+                    'value': 'StepWise',
+                    'id': '10704'
+                }
+            }
+        })
+    print (tmp)
+
+    sys.exit()
+
+    ## fieldToCust
+    tmp = [fieldToCust(i) for i in ["key", "Question Type", "MD5 Spec Hash"]]
+    print(tmp)
+    sys.exit()
+
+    ## getFields
+    flds = ["a", "c"]
+    arr = [{"a": "one", "b": "two", "c":"three"}, {"a": "four", "b": "five"}]
+    print (getFields(flds, arr))
+
+    sys.exit()
 
     ## fieldToCust
     tmpField = "ID Number"
