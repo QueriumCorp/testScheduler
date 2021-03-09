f2f51096f8f8dbb8b62afc97fdc9a55cf6a42977
Evan <igobyE@gmail.com>
2021-03-09 17:36:25 -0600

    Code clean up

--- main.py
+++ main.py
@@ -144,14 +144,3 @@ if __name__ == '__main__':
             logging.error(msgErr)
         else:
             rslt = schedule.task(aTask)
-            # if rslt["status"] == False:
-            #     logging.error("Task {id} failed: {msg}".format(
-            #         id=aTask["id"], msg=aTask["result"]))
-            # else:
-            #     ## Update status to running
-            #     task.modStts(aTask["id"], "success",
-            #         cols=["finished"], vals=[datetime.utcnow()])
-
-            #     logging.info("Task {id} completed successfully".format(
-            #         id=aTask["id"]
-            #     ))
